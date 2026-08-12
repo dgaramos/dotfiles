5f538cf739af9786d560ee21028d572bcf4f25fa
Danilo Ramos <dgaramos@gmail.com>
2026-08-12 11:57:34 -0300

    feat(zsh): add czr alias to reload shell

--- private_dot_config/zsh/common.zsh
+++ private_dot_config/zsh/common.zsh
@@ -46,6 +46,7 @@ alias czd='chezmoi diff'
 alias cza='chezmoi apply'
 alias czu='chezmoi update'
 alias cze='chezmoi edit'
+alias czr='exec zsh'
 
 if command -v starship >/dev/null 2>&1; then
     eval "$(starship init zsh)"
