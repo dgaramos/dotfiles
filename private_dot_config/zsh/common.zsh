a111e093ba18ed96e50a79bfa443a9cc4e8e1d80
Danilo Ramos <dgaramos@gmail.com>
2026-08-12 11:44:00 -0300

    refactor(zsh): move GitHub CLI aliases from work profile to common

--- private_dot_config/zsh/common.zsh
+++ private_dot_config/zsh/common.zsh
@@ -32,6 +32,14 @@ alias gd='git diff'
 alias gb='git branch'
 alias gco='git checkout'
 
+# GitHub CLI
+if command -v gh >/dev/null 2>&1; then
+    alias prs='gh pr list'
+    alias mypr='gh pr list --author=@me'
+    alias prv='gh pr view --web'
+    alias issues='gh issue list'
+fi
+
 # Chezmoi
 alias cz='chezmoi'
 alias czd='chezmoi diff'
--- private_dot_config/zsh/work.zsh
+++ private_dot_config/zsh/work.zsh
@@ -4,9 +4,3 @@
 
 # Intentionally contains no personal credentials or infrastructure secrets.
 
-if command -v gh >/dev/null 2>&1; then
-    alias prs='gh pr list'
-    alias mypr='gh pr list --author=@me'
-    alias prv='gh pr view --web'
-    alias issues='gh issue list'
-fi
